368d2e81636e8a3998da99d3148d93267334fd1d
Tyler Porter <tyler.b.porter@gmail.com>
2020-01-13 20:32:15 -0500

    Add Rubocop config and clean up linter errors

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,48 +1,50 @@
+# frozen_string_literal: true
+
 class GamesController < ApplicationController
-	skip_before_action :verify_authenticity_token
-	require 'chessmate'
+  skip_before_action :verify_authenticity_token
+  require 'chessmate'
 
-	def index
-		@game = session[:game].nil? ? ChessMate.new : ChessMate.new(**game_params)
-		@PIECES = {
-			"BK" => "king",
-			"BQ" => "queen",
-			"BR" => "rook",
-			"BB" => "bishop",
-			"BN" => "knight",
-			"BP" => "pawn",
-			"WK" => "king",
-			"WQ" => "queen",
-			"WR" => "rook",
-			"WB" => "bishop",
-			"WN" => "knight",
-			"WP" => "pawn"
-		}
-		@history = @game.move_history
-	end
+  def index
+    @game = session[:game].nil? ? ChessMate.new : ChessMate.new(**game_params)
+    @pieces = {
+      'BK' => 'king',
+      'BQ' => 'queen',
+      'BR' => 'rook',
+      'BB' => 'bishop',
+      'BN' => 'knight',
+      'BP' => 'pawn',
+      'WK' => 'king',
+      'WQ' => 'queen',
+      'WR' => 'rook',
+      'WB' => 'bishop',
+      'WN' => 'knight',
+      'WP' => 'pawn'
+    }
+    @history = @game.move_history
+  end
 
-	def move
-		@game = session[:game].nil? ? ChessMate.new : ChessMate.new(**game_params)
-		if @game.move(move_params[:origin], move_params[:destination], test=true)
-			@game.move(move_params[:origin], move_params[:destination])
-		end
-		session[:game] = @game
-		render js: "window.location = '#{root_path}'"
-	end
+  def move
+    @game = session[:game].nil? ? ChessMate.new : ChessMate.new(**game_params)
+    if @game.move(move_params[:origin], move_params[:destination], true)
+      @game.move(move_params[:origin], move_params[:destination])
+    end
+    session[:game] = @game
+    render js: "window.location = '#{root_path}'"
+  end
 
-	def new
-		reset_session
-		redirect_to root_path
-	end
+  def new
+    reset_session
+    redirect_to root_path
+  end
 
-	private
+  private
 
-	def move_params
-		params.permit(:origin, :destination)
-	end
+  def move_params
+    params.permit(:origin, :destination)
+  end
 
-	def game_params
-		params = session[:game].deep_symbolize_keys
-		params
-	end
+  def game_params
+    params = session[:game].deep_symbolize_keys
+    params
+  end
 end
